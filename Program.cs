8bfa0c811db9491fb2a059fe3f5bccd9d06a2d59
mohammad21mohebbi <146670302+mohammad21mohebbi@users.noreply.github.com>
2024-11-08 20:06:30 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                    var data = new[]
+                   {
+            new { Id = 1, Name = "Alice", Score = 3.0 },
+            new { Id = 2, Name = "Bob", Score = 7.87 },
+            new { Id = 3, Name = "Charlie", Score = 9.001 },
+            new { Id = 4, Name = "David", Score = 11.0 },
+            new { Id = 5, Name = "Eve", Score = 13.0 },
+            new { Id = 6, Name = "Frank", Score = 8.0 },
+            new { Id = 7, Name = "Grace", Score = 9.0 },
+            new { Id = 8, Name = "Hannah", Score = 9.2 },
+            new { Id = 9, Name = "Isaac", Score = 8.86 }
+        };
+
+        foreach (var student in data)
+        {
+            int roundedScore = (int)Math.Round(student.Score);
+
+            if (roundedScore > 8)
+            {
+                Console.WriteLine("accept{0}",student);
+            }
+            else
+            {
+                Console.WriteLine("Fail{0}", student);
+            }
+
+        }
+        Console.ReadKey();
+    }
+}
+
+        }
+    
+
